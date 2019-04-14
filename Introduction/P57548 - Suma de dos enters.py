076d9d7fa0f106af8468fb465c3a472adb4ebe1f
nil.vilas <nil.vilas@est.fib.upc.edu>
2019-04-15 01:28:25 +0200

    Afegits arxius a l'apartat Introduction

---
+++ Introduction/P57548 - Suma de dos enters.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+def readNumbers(maxnums):
+    result = []
+    currnums = 0
+    # llegeix linies
+    for line in sys.stdin:
+        # ignora espais
+        for x in line.split():
+            x = int(x)
+            result.append(x)
+            currnums += 1
+            if (currnums == maxnums):
+                return result
+
+def old():
+    a = input().split(' ')
+    if (len(a) > 1):
+        print(int(a[0]) + int(a[1]))
+    else:
+        b = input().split(' ')
+        print(int(a[0]) + int(b[0]))
+
+def main():
+    entrada = readNumbers(2)
+    result = sum(entrada)
+    print(result)
+
+main()
+#old()
---
+++ Introduction/P68688 - Bon dia a tothom!.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def main():
+	print('Bon dia a tothom!')
+
+main()
