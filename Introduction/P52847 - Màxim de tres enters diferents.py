2f2acd5732c6cc6eb0ae02c7e1dae53c16447a52
nil.vilas <nil.vilas@est.fib.upc.edu>
2019-04-15 01:36:01 +0200

    Afegits exercicis a Introduction

---
+++ Introduction/P52847 - Màxim de tres enters diferents.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+def readNumbers(maxnums):
+    result = []
+    currnums = 0
+    # llegeix linies
+    for line in sys.stdin:
+        # ignora espais
+        for x in line.split():
+            x = int(x)
+            result.append(x)
+            currnums += 1
+            if (currnums == maxnums):
+                return result
+
+def main():
+    entrada = readNumbers(3)
+    result = max(entrada)
+    print(result)
+
+main()
